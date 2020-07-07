a98b22773748d8fd3494133e54e74b85615d9e74
Nikolay Yanev <nikiyanev1@gmail.com>
2020-07-07 19:24:59 +0300

    modal

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,6 +1,7 @@
 let modalBtn = document.querySelector(".read-more");
 let modalBg = document.querySelector(".modal-bg");
 let modalClose = document.querySelector(".modal-close");
+let modal = document.querySelector(".modal");
 
 modalBtn.addEventListener("click", function() {
     modalBg.classList.add("bg-active");
@@ -10,4 +11,9 @@ modalClose.addEventListener("click", function() {
     modalBg.classList.remove("bg-active");
 });
 
+window.onclick = function(event) {
+    if (event.target == modalBg) {
+        modalBg.classList.remove("bg-active");
+    }
+}
 
