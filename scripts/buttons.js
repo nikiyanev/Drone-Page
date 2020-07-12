58b18fa82ea3de849ed0eb85a837c193e18c7a6c
Nikolay Yanev <nikiyanev1@gmail.com>
2020-07-12 15:00:52 +0300

    buttons logic

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -3,6 +3,7 @@ let videosSection = document.querySelector('.videos-section');
 
 function switchVisiblePhotos() {
   if(!photosSection) return;
+
   if (getComputedStyle(photosSection).display == 'none') {
     photosSection.style.display = 'block';
     videosSection.style.display = 'none';
@@ -11,10 +12,25 @@ function switchVisiblePhotos() {
 
 function switchVisibleVideos() {
     if(!videosSection) return;
-    if (getComputedStyle(videosSection).display == 'none') {
+    
+    
+   if (getComputedStyle(videosSection).display == 'none') {
       photosSection.style.display = 'none';
       videosSection.style.display = 'block';
     }
+}
+
+  if (localStorage.key(0) === "isVideos" ) {
+    photosSection.style.display = 'none';
+    videosSection.style.display = 'block';
+
+    localStorage.removeItem("isVideos");
   }
+
+  
 document.getElementById('photos').addEventListener('click', switchVisiblePhotos);
 document.getElementById('videos').addEventListener('click', switchVisibleVideos);
+
+
+
+
---
+++ scripts/card-button.js
@@ -0,0 +1,9 @@
+ 
+
+    document.getElementById("videos-card").addEventListener("click", function(){
+    
+      localStorage.setItem("isVideos", "true");
+      
+      
+    });
+
