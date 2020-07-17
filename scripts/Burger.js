4ce66b9230eafddaf52fd01a87f72f0044206148
Nikolay Yanev <nikiyanev1@gmail.com>
2020-07-17 14:40:26 +0300

    some little changes

--- scripts/Burger.js
+++ scripts/Burger.js
@@ -2,33 +2,34 @@
 const navbar = document.querySelector('nav');
 const hamburgerButton = document.querySelector('.menu-btn');
 const navbarUl = document.querySelector('.ul-nav');
+const navbarLi = document.querySelector('.ul-nav li');
 let menuOpened = false;
 
 navbarUl.classList.add('hidden');
 
-hamburgerButton.addEventListener('click', 
+hamburgerButton.addEventListener('click', (e) => {
 
-  function myFunction(e) {
-    var x = navbarUl;
-    if (x.style.display === "block") {
-      x.style.display = "none";
-      x.style.height = "0";
-    } else {
-      x.style.display = "block";
-      setTimeout(() => {
-          x.style.height = "300px";    
-      }, 1)
-    }
-  
-  // if (!menuOpened) {
-  //   hamburgerButton.classList.add('open');
-  //   navbarUl.style.height = '100%';
-  //   menuOpened = true;
-  // } else {
-  //   hamburgerButton.classList.remove('open');
-  //   menuOpened = false;
-  // }
+  if (!menuOpened) {
+    hamburgerButton.classList.add('open');
+    navbarUl.style.height = '100%';
+    menuOpened = true;
+  } else {
+    hamburgerButton.classList.remove('open');
+    menuOpened = false;
+  }
 
-  // navbarUl.classList.toggle('hidden');
+  navbarUl.classList.toggle('hidden');
 });
 
+// function myFunction() {
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//     x.style.height = "0";
+//   } else {
+//     x.style.display = "block";
+//     setTimeout(() => {
+//         x.style.height = "200px";    
+//     }, 1)
+//   }
+// }
